056d35da522d99ffdf88fd1f4ed0d9e11daa6413
Alexey Osheychik <osheychik@gmail.com>
2026-01-05 10:32:57 +0100

    Minor adjustments

--- keyboards/tweetydabird/lotus58/keymaps/lotus/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/lotus/keymap.c
@@ -10,11 +10,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        QK_GESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPLY,        KC_MPLY, KC_6,    KC_7,            KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,            KC_I,    KC_O,    KC_P,    KC_LBRC,
-        HYPER_F19, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,            KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, TG(1),             KC_UP,   KC_N,    KC_M,            KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_RBRC),
-                                   KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,    RSFT_T(KC_ENT),  KC_BSPC, KC_RALT, RGUI_T(KC_BSLS)
+        QK_GESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MPLY,        KC_MPLY, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,
+        HYPER_F19, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, TG(1),             KC_UP,   KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_BSLS),
+                                   KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,    RSFT_T(KC_ENT), KC_BSPC, KC_RALT,  RGUI_T(KC_RBRC)
     ),
     [1] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
